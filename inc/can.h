cb573eccd9283cb768d1e718b8e9d26b096836af
Rogier Reerink <me@rogier.io>
2019-12-18 11:14:19 +0100

    Initial update.

---
+++ inc/can.h
@@ -0,0 +1,67 @@
+#ifndef _CAN_H
+#define _CAN_H
+
+/**
+ * @brief Initialize the CAN controller.
+ * @param txid The message ID used for transmission.
+ * @return void
+ *
+ * Resets the CAN controller, sets its transfer speed and its protocol revision
+ * (2.0A or 2.0B) for transmission, enables interrupts, clears all message
+ * objects and enables the CAN controller.
+ *
+ * Currently, the transfer speed is set to 1MBps, the protocol revision is set
+ * to 2.0A and no interrupts are enabled.
+ */
+void can_init(uint16_t txid);
+
+/**
+ * @brief Configure a message object to filter on a specific message ID.
+ * @param rxid The message ID to filter on.
+ * @return void
+ *
+ * Searches for an unused message object and configures it to filter the bus on
+ * messages with the message ID specified. Message objects are utilized
+ * chronologically and reception is enabled immediately.
+ * 
+ * The first message object is reserved for transmission and will not be
+ * available for reception. The transmission message object uses message ID 0
+ * and has the highest priority on the bus.
+ *
+ * Best practice is to filter messages in descending order of priority. That
+ * way, the receive function will always favor a high priority message over a
+ * low priority message.
+ *
+ * @bug Currently, filtered messages with an id of 0 are discarded when also
+ * filtering for messages with higher ids, since an id of 0 indicates that the
+ * message object is not initialized.
+ */
+void can_filter(uint16_t rxid);
+
+/**
+ * @brief Retrieve a message from the first message object with a set rx flag.
+ * @param rxid A pointer to where the message ID will be copied.
+ * @param dat A pointer to where the message will be copied.
+ * @param len A pointer to where the message length will be copied.
+ * @return void
+ *
+ * Searches through all the message objects and returns the message of the
+ * first one it finds with a set reception flag. The id, message and message
+ * length of the message object are then copied to the respective memory
+ * locations pointed to by the function's parameters. Make sure to reserve
+ * enough memory at these locations.
+ */
+void can_receive(uint16_t *rxid, uint8_t *dat, uint8_t *len);
+
+/**
+ * @brief Transmit a message on the bus.
+ * @param dat A pointer to where the message is stored.
+ * @param len The number of bytes to transmit.
+ * @return void
+ *
+ * Copies the number of bytes specified by len from the memory pointed to by
+ * dat to the transmission message object and starts transmission.
+ */
+void can_transmit(uint8_t *dat, uint8_t len);
+
+#endif // _CAN_H
---
+++ src/can.c
@@ -0,0 +1,115 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <can.h>
+
+// Convert an ID to the CAN revision 2.0A IDT-register format.
+static uint32_t _id_to_idt_2a(uint16_t id) {
+	return
+		(uint32_t)(uint8_t)(id << 5) << 16 | 
+		(uint32_t)(uint8_t)(id >> 3) << 24;
+}
+
+// Convert an ID to the CAN revision 2.0B IDT-register format.
+static uint32_t _id_to_idt_2b(uint16_t id) {
+	return
+		(uint32_t)(uint8_t)(id << 3) | 
+		(uint32_t)(uint8_t)(id >> 5) << 8 |
+		(uint32_t)(uint8_t)(id >> 13) << 16;
+}
+
+void can_init(uint16_t txid) {
+
+	// Reset CAN controller
+	CANGCON = _BV(SWRES);
+
+	// Set CAN timing bits
+	CANBT1 = 0x02;
+	CANBT2 = 0x04;
+	CANBT3 = 0x13;
+
+	// Enable interrupt(s)
+	CANGIE = _BV(ENIT);
+
+	// Initialize MOb0 (tx)
+	CANPAGE = 0x00;
+	CANSTMOB = 0x00;
+	CANCDMOB = 0x00;
+	CANIDM = 0xFFFFFFFF;
+	CANIDT = _id_to_idt_2a(txid);
+
+	// Initialize MOb1 to MOb14 (rx)
+	uint8_t dat_i;
+	for (dat_i = 1; dat_i < 14; dat_i++) {
+		CANPAGE = dat_i << 4;
+		CANSTMOB = 0x00;
+		CANCDMOB = 0x00;
+		CANIDM = 0xFFFFFFFF;
+		CANIDT = 0x00000000;
+	}
+
+	// Enable CAN controller
+	CANGCON = _BV(ENASTB);
+}
+
+void can_filter(uint16_t rxid) {
+    
+	uint8_t dat_i;
+	for (dat_i = 1; dat_i < 14; dat_i++) {
+
+		// Select MOb[i]
+		CANPAGE = dat_i << 4;
+
+		// Use MOb[i] if its id is zero (i.e. not yet set)
+		if (CANIDT == 0x00000000) {
+			CANIDT = _id_to_idt_2a(rxid);
+			CANCDMOB = _BV(CONMOB1);
+			break;
+		}
+	}
+}
+
+void can_receive(uint16_t *rxid, uint8_t *dat, uint8_t *len) {
+
+	uint8_t mob_i;
+	for (mob_i = 1; mob_i < 14; mob_i++) {
+
+		// Select MOb[i]
+		CANPAGE = mob_i << 4;
+
+		// Read MOb[i] if its reception bit has been set
+		if (CANSTMOB & _BV(RXOK)) {
+
+			// Get id
+			*rxid = CANIDT2 >> 5 | CANIDT1 << 3;
+
+			// Get message length
+			*len = CANCDMOB & 0x0F;
+
+			// Get message
+			uint8_t dat_i;
+			for (dat_i = 0; dat_i < *len; dat_i++) {
+				*(dat+dat_i) = CANMSG;
+			}
+
+			// Reset reception bit and re-enable reception
+			CANSTMOB &= ~_BV(RXOK);
+			CANCDMOB = _BV(CONMOB1);
+			break;
+		}
+	}
+}
+
+void can_transmit(uint8_t *dat, uint8_t len) {
+
+	// Select MOb0
+	CANPAGE = 0x00;
+
+	// Set message
+	uint8_t dat_i;
+	for (dat_i = 0; dat_i < len; dat_i++) {
+		CANMSG = *(dat+dat_i);
+	}
+
+	// Set message length and start transmission
+	CANCDMOB = (CANCDMOB & _BV(IDE)) | _BV(CONMOB0) | (len & 0x0F);
+}
