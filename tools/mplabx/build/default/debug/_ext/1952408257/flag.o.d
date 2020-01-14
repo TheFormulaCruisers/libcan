1a2535f4dbb12ab2fd26f1d4c350cc8c03234f04
Rogier Reerink <me@rogier.io>
2020-01-14 12:57:02 +0100

    Tools.

---
+++ tools/mplabx/build/default/debug/_ext/1952408257/flag.o.d
@@ -0,0 +1,11 @@
+build/default/debug/_ext/1952408257/flag.o.d \
+ build/default/debug/_ext/1952408257/flag.o: \
+ ../../libs/libextra/src/flag.c ../../libs/libextra/inc/extra/flag.h \
+ /usr/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /usr/microchip/xc8/v2.10/avr/avr/include/stdint.h
+
+../../libs/libextra/inc/extra/flag.h:
+
+/usr/microchip/xc8/v2.10/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/usr/microchip/xc8/v2.10/avr/avr/include/stdint.h:
